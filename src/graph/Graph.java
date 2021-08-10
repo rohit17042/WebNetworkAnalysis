cad8cae75a13cf466049791ec069e9782906c278
rohit <rohitchaudhary17042@gmail.com>
2021-08-10 20:49:17 +0530

    graph interface added

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,5 +1,34 @@
 package graph;
 
-public interface Graph {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
+/**
+ *
+ * @author: Rohit Chaudhary
+ *
+ */
+
+public interface Graph {
+    /* Creates a vertex with the given number. */
+    public void addVertex(int num);
+
+    /* Creates an edge from the first vertex to the second. */
+    public void addEdge(int from, int to);
+
+    /* Finds the egonet centered at a given node. */
+    public Graph getEgonet(int center);
+
+    /* Returns all SCCs in a directed graph. */
+    public List<Graph> getSCCs();
+
+    /* Return the graph's connections in a readable format.
+     * The keys in this HashMap are the vertices in the graph.
+     * The values are the nodes that are reachable via a directed
+     * edge from the corresponding key.
+     * The returned representation ignores edge weights and
+     * multi-edges.  */
+
+    public HashMap<Integer, HashSet<Integer>> exportGraph();
 }
